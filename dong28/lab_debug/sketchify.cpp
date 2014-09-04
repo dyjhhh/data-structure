e704b21084a68689e3317a2d8c073d77456c0f3e
dong28 <dong28@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-04 20:24:48 +0000

    lab_debug submission

--- dong28/lab_debug/sketchify.cpp
+++ dong28/lab_debug/sketchify.cpp
@@ -1,47 +1,39 @@
 #include <cstdlib>
 #include "png.h"
 
-//
+#include<iostream>  //added
+using namespace std;  //added
+
 // sets up the output image
-PNG * setupOutput(int w, int h)
-{
-	PNG * image = new PNG(w, h);
-	return image;
-}
+PNG * setupOutput(int w, int h);
 
 
 // Returns my favorite color
-RGBAPixel * myFavoriteColor(int intensity)
-{
-	RGBAPixel color;
-	color.red   = 0;
-	color.green = intensity/2;
-	color.blue  = intensity;
-	return &color;
-}
+RGBAPixel * myFavoriteColor(int intensity);
 
 
-void sketchify()
+
+int sketchify()
 {
 	// Load in.png
-	PNG * original;
-
+  PNG * original=new PNG; //added
+        
 	original->readFromFile("in.png");
 	int width  = original->width();
 	int height = original->height();
-
+         
 	// Create out.png
-	PNG * output;
-	setupOutput(width, height);	
+	PNG * output=new PNG;
+	output -> resize (width, height);	
 
 	// Loud our favorite color to color the outline
 	RGBAPixel * myPixel = myFavoriteColor(192);
 
 	// Go over the whole image, and if a pixel differs from that to its upper
 	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
+	for (int y = 1;  y < height; y++)
 	{
-		for (int x = 1; 0 < x < width; x++)
+		for (int x = 1;  x < width; x++)
 		{
 			// Calculate the pixel difference
 			RGBAPixel * prev = (*original)(x-1, y-1);
@@ -54,7 +46,8 @@ void sketchify()
 			// color the output pixel with my favorite color
 			RGBAPixel * currOutPixel = (*output)(x,y);
 			if (diff > 100)
-				currOutPixel = myPixel;
+			  //cout <<"line 47"<< endl                          
+				*currOutPixel =* myPixel;
 		}
 	}
 	
@@ -65,4 +58,27 @@ void sketchify()
 	delete myPixel;
 	delete output;
 	delete original;
+        myPixel = NULL;
+        output = NULL;
+        original = NULL;
+	return 0;
+	cout << "reached line 74" << endl;
+}
+
+// sets up the output image
+PNG * setupOutput (int w, int h)
+{
+            PNG * image = new PNG (w, h);
+            return image;
+
+}
+
+
+//returns my favorite color
+RGBAPixel * myFavoriteColor (int intensity)
+{RGBAPixel* color = new RGBAPixel;
+  color -> red =0;
+  color ->green = intensity/2;
+  color ->blue = intensity;
+  return color;
 }
