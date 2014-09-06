46ddd632c59968883841767b90a7e4e428096819
dong28 <dong28@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-06 21:05:34 +0000

    mp1 submission

--- dong28/mp1/main.cpp
+++ dong28/mp1/main.cpp
@@ -1,30 +1,26 @@
-#include <iostream>
-#include "rgbapixel.h"
 #include "png.h"
-
-using namespace std;
 
 int main()
 {
-  PNG oldImage;
-  oldImage.readFromFile("in.png");
-  size_t vertical = oldImage.height();
-  size_t horizontal = oldImage.width();
+	PNG inImage("in.png");
+	int height= inImage.height();
+	int width= inImage.width();
+	PNG outImage(width, height);
 
-	//have initialized newImage just so that the pixels are accessible
-
-  PNG newImage;
-  newImage.readFromFile("in.png");
-	
-  //we will flip the image horizontally and vertically to rotate it by 180 degrees
-  for(size_t x = 0; x < oldImage.width(); x++)
+	for (int i = 0; i < width; i++) 
 	{
-		for(size_t y = 0; y < oldImage.height(); y++)
+		for (int j = 0; j < height; j++)
 		{
-       	newImage(horizontal-x-1, vertical-y-1)->red = oldImage(x,y)->red;
-	newImage(horizontal-x-1, vertical-y-1)->green = oldImage(x,y)->green;
-        newImage(horizontal-x-1, vertical-y-1)->blue = oldImage(x,y)->blue;
-	        }
-        }
-	newImage.writeToFile("out.png");
-}	
+			RGBAPixel a = *inImage(width-i-1,height-j-1);
+			outImage(i,j)->red=a.red;
+			outImage(i,j)->green=a.green;
+			outImage(i,j)->blue=a.blue;
+			outImage(i,j)->alpha=a.alpha;	
+		}
+	}
+	
+	outImage.writeToFile("out.png");
+
+	return 0;	
+
+}
