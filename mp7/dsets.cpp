ed29dbd78b40b03b133a5d9405400b9d8499e9b1
dong28 <dyjhhh@users.noreply.github.com>
2014-12-03 05:17:30 +0000

    mp7.1 submission

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -5,31 +5,28 @@ void DisjointSets:: addelements(size_t number)
 {
 	max_represtative=0;
         int offset=myvector.size();
-	for(unsigned int i=offset;i<number+offset;i++)
+	for(unsigned int a=offset;a<number+offset;a++)
 		myvector.push_back(-1);	
 	
-	//cout<<" "<<mymap[number]<<"\n";
-	
-}
-void DisjointSets::setunion(size_t  element1,size_t element2)
+	}
+void DisjointSets::setunion(size_t element1,size_t element2)
 {
-	//cout<<"elements"<<element1<<" "<<element2<<"\n";
-	if( element1<myvector.size() && element2<myvector.size())
+       	if( element1<myvector.size() && element2<myvector.size())
 	{
 		element1=find(element1);
 		element2=find(element2);
-		int newsize=myvector[element1] +  myvector[element2];
-		if(-(newsize)>=max_represtative)
-			max_represtative=-newsize;
+		int size=myvector[element1] +  myvector[element2];
+		if(-(size)>=max_represtative)
+			max_represtative=-size;
 		if(element1>element2)
 		{
 			myvector[element2]=element1;
-			myvector[element1]=newsize;
+			myvector[element1]=size;
 		}
 		else
 		{
 			myvector[element1]=element2;
-			myvector[element2]=newsize;
+			myvector[element2]=size;
 		}	
 	}
 }
@@ -52,14 +49,10 @@ int DisjointSets::find(size_t number)
 
 bool DisjointSets:: no_moreDsets()
 {
-	//cout<<max_represtative;
   if(max_represtative>=0)
 {
   if((unsigned) max_represtative==myvector.size())
 	{
-		//for(int i=0;i<myvector.size();i++)
-		//	cout<<" "<<myvector[i];
-		//cout<<"\n";	
 		return true;
 	}
 	else
